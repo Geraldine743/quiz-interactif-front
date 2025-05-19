4b688a3770cb2af8582988ed9d9bc92a4cabcddb
Geraldine743 <egeraldine2@gmail.com>
2025-05-19 15:07:07 +0200

    refactoring function submitQuiz

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function submitQuiz(){
+function calculateScore (callback){
     const correctAnswers = {
         q1:"Paris",
         q2:"Mercure",
@@ -14,13 +14,34 @@ function submitQuiz(){
             score++
         }
     }
+    callback(score)
+}
+
+function displayResult(score, callback){
     const resultDiv = document.getElementById("result")
     resultDiv.innerHTML = `Votre score est de ${score}.`
+    callback(score)
+}
+
+function handleMessage(score){
+    const resultDiv = document.getElementById("result")
+    resultDiv.classList.remove("excellent","good","try-again")
     if(score === 3){
         resultDiv.innerHTML += "<br> Excellent !"
+        resultDiv.classList.add("excellent")
     }else if (score === 2){
         resultDiv.innerHTML += "<br> Bon travail, vous pouvez vous améliorer !"
+        resultDiv.classList.add("good")
     }else{
         resultDiv.innerHTML += "<br> Vous pouvez faire mieux !"
+        resultDiv.classList.add("try-again")
     }
+}
+
+function submitQuiz(){
+    calculateScore(function(score){
+        displayResult(score, function(){
+            handleMessage(score)
+        })
+    })
 }
