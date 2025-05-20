45432247c5be1fa85b65d9fe349d27d7db3a7ed8
Geraldine743 <egeraldine2@gmail.com>
2025-05-20 09:09:17 +0200

    add function loginUser()

--- script.js
+++ script.js
@@ -60,3 +60,19 @@ function registerUser(){
         alert("Veuillez remplir tous les champs.")
     }
 }
+
+//Connexion
+function loginUser(){
+    const username = document.getElementById ("login-username").value
+    const password = document.getElementById ("login-password").value
+
+    const storedUsername = localStorage.getItem("username")
+    const storedPassword = localStorage.getItem("password")
+
+    if(username === storedUsername && password === storedPassword){
+        localStorage.setItem("isAuthenticated",true)
+        window.location.href = "index.html"
+    }else{
+        alert("Nom d'utilisateur ou Mot de passe incorrecte.")
+    }
+}
